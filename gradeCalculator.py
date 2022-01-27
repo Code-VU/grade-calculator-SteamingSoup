e974e8179b765eb8dac271f0c27e40280b252b7d
Nicholas Stein <Nicholas.Stein@vu.com>
2022-01-27 11:38:28 -0600

    Adjusted ranges

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -5,17 +5,16 @@ def calculateGrade():
     try:
         hrs = float(input("Enter score:"))
 
-        for i in range(0.0, 1.0):
-            if hrs >= 0.9:
-                print('A')
-            elif hrs >= 0.8:
-                print('B')
-            elif hrs >= 0.7:
-                print('C')
-            elif hrs >= 0.6:
-                print('D')
-            elif hrs < 0.6:
-                print('F')
+        if hrs >= 0.9 and hrs <= 1.0:
+            print('A')
+        elif hrs >= 0.8:
+            print('B')
+        elif hrs >= 0.7:
+            print('C')
+        elif hrs >= 0.6:
+            print('D')
+        elif hrs < 0.6 and hrs >= 0.0:
+            print('F')
     
     except:
         print('Bad Score')
@@ -26,4 +25,4 @@ def calculateGrade():
 ## uncomment calculateGrade() and run > python calculateGrade.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-## calculateGrade()
+calculateGrade()
